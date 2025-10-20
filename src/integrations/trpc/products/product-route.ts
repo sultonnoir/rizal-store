857e36a910588e511749f8c814029bc13cef9fc6
sultonnoir <sultonoir@gmail.com>
2025-10-20 14:00:00 +0700

    test : product router

--- src/integrations/trpc/products/product-route.ts
+++ src/integrations/trpc/products/product-route.ts
@@ -3,7 +3,7 @@ import { publicProcedure } from "../init";
 import { searchProducts, searchProductSchema } from "./product-service";
 
 export const productRouter = {
-  list: publicProcedure
-    .input(searchProductSchema)
-    .query(async ({ input }) => await searchProducts(input)),
+  list: publicProcedure.input(searchProductSchema).query(async ({ input }) => {
+    return await searchProducts(input);
+  }),
 } satisfies TRPCRouterRecord;
